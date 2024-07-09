2fb2ed137e2b6342ef39f9249f505c329aabf749
Cass <cassianoleaoramos@gmail.com>
2024-07-09 12:52:50 -0300

    initial project setup

---
+++ src/test/java/cass/nlw_trips/TripsApplicationTests.java
@@ -0,0 +1,13 @@
+package cass.nlw_trips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
