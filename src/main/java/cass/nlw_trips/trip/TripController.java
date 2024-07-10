f66a36a0ffb77cec61a956b748db0a203922d899
Cass <cassianoleaoramos@gmail.com>
2024-07-10 18:59:38 -0300

    feat: implement participants invite, trip confirmation and get all participants from trip

--- src/main/java/cass/nlw_trips/trip/TripController.java
+++ src/main/java/cass/nlw_trips/trip/TripController.java
@@ -1,11 +1,17 @@
 package cass.nlw_trips.trip;
 
+import cass.nlw_trips.participant.DTOs.ParticipantCreateResponse;
+import cass.nlw_trips.participant.DTOs.ParticipantData;
+import cass.nlw_trips.participant.DTOs.ParticipantRequestPayload;
 import cass.nlw_trips.participant.ParticipantService;
 import cass.nlw_trips.trip.DTOs.TripCreateResponse;
 import cass.nlw_trips.trip.DTOs.TripRequestPayload;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,9 +34,32 @@ public class TripController {
 
         this.tripRepository.save(newTrip);
 
-        this.participantService.registerParticipantsToTrip(tripRequestPayload.emails_to_invite(), newTrip.getId());
+        this.participantService.registerParticipantsToTrip(tripRequestPayload.emails_to_invite(), newTrip);
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @PostMapping("/{id}/invite")
+    public ResponseEntity<ParticipantCreateResponse> inviteParticipant(@PathVariable UUID id,
+                                                                       @RequestBody ParticipantRequestPayload participantRequestPayload) {
+        Optional<Trip> trip = tripRepository.findById(id);
+
+        if (trip.isPresent()) {
+            Trip createdTrip = trip.get();
+
+            ParticipantCreateResponse participantCreateResponse = this.participantService
+                    .registerParticipantToTrip(
+                            participantRequestPayload.email(),
+                            createdTrip
+                    );
+
+            if (createdTrip.isConfirmed()) {
+                participantService.triggerConfirmationEmailToParticipant(participantRequestPayload.email());
+            }
+
+            return ResponseEntity.ok(participantCreateResponse);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/{id}")
@@ -43,4 +72,47 @@ public class TripController {
                         .notFound()
                         .build());
     }
+
+    @GetMapping("/{id}/confirm")
+    public ResponseEntity<Trip> confirmTrip(@PathVariable UUID id) {
+        Optional<Trip> trip = this.tripRepository.findById(id);
+
+        if (trip.isPresent()) {
+            Trip confirmationTrip = trip.get();
+            confirmationTrip.setConfirmed(true);
+
+            this.tripRepository.save(confirmationTrip);
+            this.participantService.triggerConfirmationEmailToParticipants(id);
+
+            return ResponseEntity.ok(confirmationTrip);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/participants")
+    public ResponseEntity<List<ParticipantData>> getAllParticipantsFromTrip(@PathVariable UUID id) {
+        List<ParticipantData> participantList = participantService.getAllParticipantsFromTrip(id);
+
+        return ResponseEntity.ok(participantList);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripRequestPayload tripRequestPayload) {
+        Optional<Trip> trip = this.tripRepository.findById(id);
+
+        if (trip.isPresent()) {
+            Trip updatedTrip = trip.get();
+
+            updatedTrip.setStartsAt(LocalDateTime.parse(tripRequestPayload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+            updatedTrip.setEndsAt(LocalDateTime.parse(tripRequestPayload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+            updatedTrip.setDestination(tripRequestPayload.destination());
+
+            this.tripRepository.save(updatedTrip);
+
+            return ResponseEntity.ok(updatedTrip);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
 }
