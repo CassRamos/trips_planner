8d391d6baec8d73c0ce59fac71ce0792dd27220c
Cass <cassianoleaoramos@gmail.com>
2024-07-10 15:21:54 -0300

    feat: add `Participant` entity and create table `participants`

---
+++ src/main/java/cass/nlw_trips/participant/Participant.java
@@ -0,0 +1,32 @@
+package cass.nlw_trips.participant;
+
+import cass.nlw_trips.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "participants")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
